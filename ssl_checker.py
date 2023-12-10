c87b88d751e496a2455d4da29e53d84e1cff58ff
Abhinav Mishra <abhinav@bureau.id>
2023-12-10 14:11:19 +0530

    .

--- ssl_checker.py
+++ ssl_checker.py
@@ -1,11 +1,17 @@
-import requests
+import certifi
+import ssl
+import socket
 
 
-def verify_ssl_certificate(url):
-    response = requests.get(url)
-    response.raise_for_status()
-    print("Certificate is valid.")
+def verify_ssl_certificate(hostname):
+    context = ssl.create_default_context(cafile=certifi.where())
+
+    with socket.create_connection((hostname, 443)) as sock:
+        with context.wrap_socket(sock, server_hostname=hostname) as ssock:
+            ssock.do_handshake()
+            cert = ssock.getpeercert()
+            print("Certificate is valid.")
 
 
 if __name__ == "__main__":
-    verify_ssl_certificate("https://iabhinav.in")
+    verify_ssl_certificate("iabhinav.in")
